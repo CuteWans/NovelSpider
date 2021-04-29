82525c384a0424a559c24563d1a18aeaf3ccdead
Wans <792627087@qq.com>
2021-04-29 18:56:34 +0800

    v1.0.3 修复了多次点击button程序闪退的问题

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,9 +3,7 @@
 
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+    : QMainWindow(parent), ui(new Ui::MainWindow), fd(nullptr) {
     ui->setupUi(this);
     connect(ui->pushButton, &QPushButton::clicked, this, &MainWindow::Getessay);
 }
@@ -13,13 +11,12 @@ MainWindow::MainWindow(QWidget *parent)
 void MainWindow::Getessay() {
     const QString url_str = ui->textEdit->toPlainText();
     QUrl url(url_str);
+    if(fd != nullptr)   delete fd;
     fd = new FileDownloader(url, this);
-
     connect(fd, SIGNAL (downloaded()), this, SLOT (loadText()));
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
 
@@ -27,5 +24,4 @@ void MainWindow::loadText() {
     QTextCodec *tc = QTextCodec::codecForName("GBK");
     QString str = tc -> toUnicode(fd->downloadedData());
     ui->textBrowser->setPlainText(str);
-    fd->deleteLater();
 }
